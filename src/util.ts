303a0a5ce34657a5596cc682a1fc4ec504c7f671
Andrew van Rooyen <wraith.andrew@gmail.com>
2021-03-06 15:58:11 +0200

    batch review extraction to cohorts of 50

--- src/util.ts
+++ src/util.ts
@@ -27,3 +27,17 @@ export const exit = (error?: Error) => {
 }
 
 process.on('SIGINT', () => exit())
+
+export async function batchedPromiseAll<A, R>(
+  fn: (...args: A[]) => Promise<R>,
+  args: A[][], batchSize: number
+): Promise<R[]> {
+  let position = 0
+  let results = []
+  while (position < args.length) {
+    const argsForBatch = args.slice(position, position + batchSize)
+    results = [...results, ...await Promise.all(argsForBatch.map(args => fn(...args)))]
+    position += batchSize
+  }
+  return results
+}
